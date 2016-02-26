4313c22d26e42bfa2269b35556e41b1cdb29fd79
lbuser <lbuser@noreply.github.com>
2016-02-26 16:41:14 -0500

    generate txt files for plotting

--- image_generation/src/image-generation.jl
+++ image_generation/src/image-generation.jl
@@ -1,26 +1,41 @@
 using DataFrames
 
-function map_roi_vals(roi_locations_path::ASCIIString,
-                      roi_map::Dict{Int64, Float64},
-                      dest_path::ASCIIString)
+
+function fill_brain(roi_locs::DataFrame)
+  df_right = by(roi_locs, :roi) do df
+    roi = string("R", df[:roi][1])
+    DataFrame(x=df[:x], y=df[:y], z=df[:z], tmp=roi)
+  end
+  df_left = by(roi_locs, :roi) do df
+    roi = string("L", df[:roi][1])
+    DataFrame(x=-1*df[:x], y=df[:y], z=df[:z], tmp=roi)
+  end
+  ret = vcat(df_left, df_right)
+  delete!(ret, :roi)
+  rename!(ret, :tmp, :roi)
+end
+
+
+function map_roi_vals(data_col::Symbol,
+                      roi_mets::DataFrame = readtable("../../analysis/output/local.csv"),
+                      roi_locations_path::ASCIIString="../data/cluster_rois_4D.txt")
   roi_locs::DataFrame = begin
-    df = DataFrame(readdlm(roi_locations_path, header = false))
-    df[:x4] = round(Int64, df[:x4])
-    df
+    df = readtable(roi_locations_path, names=[:x, :y, :z, :roi], separator=' ')
+    ret = fill_brain(df)
   end
 
-  ret = by(roi_locs, :x4) do df
-    x4::Int64 = df[:x4][1]
-    new_x4 = roi_map[x4]
-    DataFrame(x1 = df[:x1], x2=df[:x2], x3=df[:x3], tmp=new_x4)
+  ret = by(roi_locs, :roi) do df
+    roi::ASCIIString = df[:roi][1]
+    roi_val::Float64 = roi_mets[roi_mets[:node] .== roi, data_col][1]
+    DataFrame(x = df[:x], y=df[:y], z=df[:z], roi_val=roi_val)
   end
 
   begin
-    delete!(ret, :x4)
-    rename!(ret, :tmp, :x4)
+    delete!(ret, :roi)
   end
 
-  writetable(dest_path, ret, header=false, separator=' ')
+  dest_path = string("../output/", data_col, ".txt")
+  writetable(dest_path, ret, separator=' ', header=false)
 
   ret
 end
